f5b67c02c8c5dbb8dc8566705aa85f69206e3195
Ram ratan yadav <rratan100@gmail.com>
2024-12-28 00:00:11 +0530

    Update main.tf

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -58,6 +58,13 @@ resource "aws_security_group" "mysql_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+  from_port   = -1
+  to_port     = -1
+  protocol    = "icmp"
+  cidr_blocks = ["10.0.2.0/24"]  # Replace with the CIDR block of the Bastion host
+}
+
   egress {
     from_port   = 0
     to_port     = 0
