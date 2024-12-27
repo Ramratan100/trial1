8d5c0ccb64ac54b7bc0e6a779ebbd631237ce11b
Ram ratan yadav <rratan100@gmail.com>
2024-12-27 23:58:22 +0530

    Update main.tf

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -28,6 +28,44 @@ resource "aws_subnet" "private_subnet_database" {
   }
 }
 
+# NAT Gateway for Private Subnet
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "NAT-Gateway-EIP"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_web.id
+
+  tags = {
+    Name = "NAT-Gateway"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.database_vpc.id
+
+  tags = {
+    Name = "Private-Route-Table-Database"
+  }
+}
+
+resource "aws_route" "private_to_internet" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet_database.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.database_vpc.id
 
@@ -40,4 +78,9 @@ resource "aws_route" "public_internet_route" {
   route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.database_igw.id
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet_web.id
+  route_table_id = aws_route_table.public_route_table.id
 }
