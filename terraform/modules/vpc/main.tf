2e6d103e9cb8f042abb00d0a69704c5144d3fbfe
Ram ratan yadav <rratan100@gmail.com>
2024-12-27 23:48:43 +0530

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "database_vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = var.database_vpc_name
+    Name = "Database-VPC"
   }
 }
 
@@ -16,4 +16,18 @@ resource "aws_vpc_peering_connection" "vpc_peering" {
   tags = {
     Name = "Master-Database-VPC-Peering"
   }
+}
+
+# Route Table for Master EC2 VPC to Database VPC
+resource "aws_route" "master_to_database" {
+  route_table_id         = "rtb-0c82564b54a7fa492"  # Replace with your Master EC2 VPC route table ID
+  destination_cidr_block = aws_vpc.database_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+}
+
+# Route Table for Database VPC to Master EC2 VPC
+resource "aws_route" "database_to_master" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "172.31.0.0/16"  # Master EC2 VPC CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
 }
