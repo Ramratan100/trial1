0d92603e566cef6cc0cbc9015903237c42178319
Ram ratan yadav <rratan100@gmail.com>
2024-12-28 00:53:56 +0530

    Create variables.tf

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,14 @@
+variable "az" {
+  description = "Availability zone"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
