5bb2b886853610fb95bff6a3cdaea332aa06df7e
Ram ratan yadav <rratan100@gmail.com>
2024-12-27 14:16:55 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "private1" {
+resource "aws_instance" "private" {
   ami               = var.ami
   instance_type     = "t2.micro"
   subnet_id         = var.private_subnet_id
@@ -6,28 +6,6 @@ resource "aws_instance" "private1" {
   vpc_security_group_ids = [var.securitygroup_id]  # Correct argument for VPC
   tags = {
     Name = "Private1-EC2"
-  }
-}
-
-resource "aws_instance" "private2" {
-  ami               = var.ami
-  instance_type     = "t2.micro"
-  subnet_id         = var.private_subnet_id
-  key_name          = var.key_name
-  vpc_security_group_ids = [var.securitygroup_id]  # Correct argument for VPC
-  tags = {
-    Name = "Private2-EC2"
-  }
-}
-
-resource "aws_instance" "private3" {
-  ami               = var.ami
-  instance_type     = "t2.micro"
-  subnet_id         = var.private_subnet_id
-  key_name          = var.key_name
-  vpc_security_group_ids = [var.securitygroup_id] # Correct argument for VPC
-  tags = {
-    Name = "Private3-EC2"
   }
 }
 
